80153b25757a5ab45d33d5cad88704f8305ff35f
Karsten Bott <Karsten.Bott@bottnet.de>
2017-05-18 08:45:45 +0200

    format

--- PSlibstorage/PSlibstorage.psm1
+++ PSlibstorage/PSlibstorage.psm1
@@ -1,154 +1,142 @@
 
-function Get-libStorageVersion
-{
+function Get-libStorageVersion {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
-$uri = "https://$($libstoragehost):$($libstorageport)/help/$myself"
-invoke-libStorageRequest -uri $uri 
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
+    $uri = "https://$($libstoragehost):$($libstorageport)/help/$myself"
+    invoke-libStorageRequest -uri $uri 
 }
 
-function Get-libStorageVolumes
-{
+function Get-libStorageVolumes {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
-$uri = "https://$($libstoragehost):$($libstorageport)/$myself"
-invoke-libStorageRequest -uri $uri 
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
+    $uri = "https://$($libstoragehost):$($libstorageport)/$myself"
+    invoke-libStorageRequest -uri $uri 
 }
-function Get-libStorageExecutors
-{
+function Get-libStorageExecutors {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
-$uri = "https://$($libstoragehost):$($libstorageport)/$myself"
-invoke-libStorageRequest -uri $uri 
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
+    $uri = "https://$($libstoragehost):$($libstorageport)/$myself"
+    invoke-libStorageRequest -uri $uri 
 }
-function Get-libStorageTasks
-{
+function Get-libStorageTasks {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
-$uri = "https://$($libstoragehost):$($libstorageport)/$myself"
-invoke-libStorageRequest -uri $uri 
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
+    $uri = "https://$($libstoragehost):$($libstorageport)/$myself"
+    invoke-libStorageRequest -uri $uri 
 }
 
-function Get-libStorageServices
-{
+function Get-libStorageServices {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
-$uri = "https://$($libstoragehost):$($libstorageport)/$myself"
-invoke-libStorageRequest -uri $uri 
+    $Myself = ($MyInvocation.MyCommand.Name.Substring(14)).ToLower()
+    $uri = "https://$($libstoragehost):$($libstorageport)/$myself"
+    invoke-libStorageRequest -uri $uri 
 }
-function Get-libStorageFunctions
-{
+function Get-libStorageFunctions {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true,Position = 1)]$libstoragehost,
-        [Parameter(Mandatory=$false)]$libstorageport = 7980
-		#[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		#[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		#[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true, Position = 1)]$libstoragehost,
+        [Parameter(Mandatory = $false)]$libstorageport = 7980
+        #[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
+        #[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
+        #[Parameter(Mandatory=$false)]$Body
     )
-$uri = "https://$($libstoragehost):$($libstorageport)"
-invoke-libStorageRequest -uri $uri 
+    $uri = "https://$($libstoragehost):$($libstorageport)"
+    invoke-libStorageRequest -uri $uri 
 }  
 
-function invoke-libStorageRequest
-{
+function invoke-libStorageRequest {
     [CmdletBinding(HelpUri = "http://pslibstorage.readthedocs.io/en/latest")]
     #[OutputType([int])]
     Param
     (
         # Param1 help description
-        [Parameter(Mandatory=$true)]$uri,
-		[Parameter(Mandatory=$false)][ValidateSet('Get','Delete','Put','Post','Patch')]$Method = 'Get',
-		[Parameter(Mandatory=$false)]$ContentType = 'application/json;charset=utf-8', 
-		[Parameter(Mandatory=$false)]$Body
+        [Parameter(Mandatory = $true)]$uri,
+        [Parameter(Mandatory = $false)][ValidateSet('Get', 'Delete', 'Put', 'Post', 'Patch')]$Method = 'Get',
+        [Parameter(Mandatory = $false)]$ContentType = 'application/json;charset=utf-8', 
+        [Parameter(Mandatory = $false)]$Body
     )
 
-$response = Invoke-RestMethod -Uri $uri -Method $Method  -SkipCertificateCheck -ContentType $ContentType
+    $response = Invoke-RestMethod -Uri $uri -Method $Method  -SkipCertificateCheck -ContentType $ContentType
 
 
-if ($names = $response |Get-Member -MemberType NoteProperty | where Definition -match "System.Management.Automation.PSCustomObject")
-    {
+    if ($names = $response |Get-Member -MemberType NoteProperty | Where-Object Definition -match "System.Management.Automation.PSCustomObject") {
         expand-libstorageproperty -prop_names $names -prop_elements $response
     }
-else
-    {
+    else {
         Write-Output $response
     }
 }
 
 
-function expand-libstorageproperty
-{
-Param(
-$prop_names,
-$prop_elements)
+function expand-libstorageproperty {
+    Param(
+        $prop_names,
+        $prop_elements)
 
 
-$output_element = @()
+    $output_element = @()
     foreach ($prop_name in $prop_names.name) {
         $elements = $prop_elements.("$prop_name")
-        if ($elements_names = $elements | Get-Member -MemberType NoteProperty | where Definition -Match "System.Management.Automation.PSCustomObject")
-            {
-                expand-libstorageproperty -prop_name $elements_names -prop_elements $elements
-            }
-       else
-        {
+        if ($elements_names = $elements | Get-Member -MemberType NoteProperty | Where-Object Definition -Match "System.Management.Automation.PSCustomObject") {
+            expand-libstorageproperty -prop_name $elements_names -prop_elements $elements
+        }
+        else {
             $output_element += $elements
         }      
 
     }
-Write-Output $output_element    
+    Write-Output $output_element    
 }
